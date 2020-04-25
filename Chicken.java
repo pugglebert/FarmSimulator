9aabde7b42ba4c9d920737616e688c51015d25f7
frostydj <64118807+frostydj@users.noreply.github.com>
2020-04-25 20:40:10 +1200

    Add files via upload

---
+++ Chicken.java
@@ -0,0 +1,13 @@
+
+public class Chicken extends Animals{
+	private int buyPrice = 0;
+	private int baseReturn = 0;
+	
+	public int getBuyPrice() {
+		return buyPrice;
+	}
+	
+	public int getBaseReturn() {
+		return baseReturn;
+	}
+}
