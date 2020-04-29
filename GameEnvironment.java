23cc3ef27dfccd40bfb5ec1f225951f520021f42
frostydj <64118807+frostydj@users.noreply.github.com>
2020-04-29 17:36:01 +1200

    Update GameEnvironment.java

--- GameEnvironment.java
+++ GameEnvironment.java
@@ -17,25 +17,37 @@ public class GameEnvironment {
 		while(actions <= 2) {
 		
 			System.out.print("Enter a number: ");
-	        Scanner scanner = new Scanner(System. in);
-	        String option = scanner. nextLine();
-	        
-	        switch(option) {
-	        case 1: /* view status farm*/
-	        case 2: /* view status crops*/
-	        case 3: /* view status animals*/
-	        case 4: /*visit store*/
-	        case 5: nextDay();
-	        case 6: /*tend to crops*/
-	        	actions++;
-	        case 7: /*play with animals*/
-	        	actions++;
-	        case 8: /*harvest crops*/
-	        	actions++;
-	        case 9: /*tend to farm land*/
-	        	actions++;
-	        case 10: /*feed animals*/
-	        	actions++;
+			Scanner scanner = new Scanner(System. in);
+			String option = scanner. nextLine();
+
+			switch(option) {
+			case "1": /* view status farm*/
+				break;
+			case "2": /* view status crops*/
+				break;
+			case "3": /* view status animals*/
+				break;
+			case "4": /*visit store*/
+				break;
+			case "5": 
+				nextDay();
+				break;
+			case "6": /*tend to crops*/
+				actions++;
+				break;
+			case "7": /*play with animals*/
+				actions++;
+				break;
+			case "8": /*harvest crops*/
+				actions++;
+				break;
+			case "9": /*tend to farm land*/
+				actions++;
+				break;
+			case "10": /*feed animals*/
+				actions++;
+				break;
+			}
         }
 	}
 	
