9113fd3cde6cc17f4ea8980c33ebaf193c53a9b4
frostydj <64118807+frostydj@users.noreply.github.com>
2020-05-02 15:57:10 +1200

    Update Crop.java

--- Crop.java
+++ Crop.java
@@ -3,21 +3,51 @@ package farmSimulator;
 public class Crop {
 	private int harvestAge;
 	private int age = 0;
+	private int buyPrice;
+	private int sellPrice;
+	private String cropType;
+	
+	Crop(String newCropType, int newHarvestAge, int newBuyPrice, int newSellPrice){
+		cropType = newCropType;
+		harvestAge = newHarvestAge;
+		buyPrice = newBuyPrice;
+		sellPrice = newSellPrice;
+	}
+	
+	public String getCropType() {
+		return cropType;
+	}
+	
+	public int getBuyPrice() {
+		return buyPrice;
+	}
+
+	public int getSellPrice() {
+		return sellPrice;
+	}
+
+	public int getHarvestAge() {
+		return harvestAge;
+	}
+	
+	public void decreaseHarvestAge(int amount) {
+		harvestAge -= amount;
+		
+		if (harvestAge < 1) {
+			harvestAge = 1;
+		}
+	}
 	
 	public void advanceDay() {
 		age++;
 	}
 	
 	public boolean canHarvest() {
-		if (age == harvestAge) {
+		if (age >= harvestAge) {
 			return true;
 		}
 		else {
 			return false;
 		}
-	}
-	
-	public void boostGrowth(int days) {
-		age += days;
 	}	
 }
