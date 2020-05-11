4e1a76f7367eeb651fb190293038017482ca72a9
frostydj <64118807+frostydj@users.noreply.github.com>
2020-05-11 14:03:44 +1200

    Update Item.java

--- Item.java
+++ Item.java
@@ -4,12 +4,25 @@ abstract class Item implements Buyable {
 	
 	private String name;
 	private int price;
+	private int inventory_count = 0;
 	
 	public abstract boolean isFoodItem();
 	public abstract boolean isCropItem();
 	public abstract String toString();
 	public abstract String toStringStore();
 	
+	
+	public void addToInventory() {
+		inventory_count++;
+	}
+	
+	public void removeFromInventory() {
+		inventory_count--;
+	}
+	
+	public int getInventoryCount() {
+		return inventory_count;
+	}
 	
     public void setPrice(int newPrice) {
     	price = newPrice;
