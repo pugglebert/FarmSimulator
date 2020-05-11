e04e60bbc91ba7285746dd9492fd61e5883c836f
frostydj <64118807+frostydj@users.noreply.github.com>
2020-05-11 14:28:46 +1200

    Add files via upload

--- Item.java
+++ Item.java
@@ -1,15 +1,30 @@
 package farmSimulator;
 
+import farmSimulatorGUI.Buyable;
+
 abstract class Item implements Buyable {
 	
 	private String name;
 	private int price;
+	private int inventory_count = 0;
 	
 	public abstract boolean isFoodItem();
 	public abstract boolean isCropItem();
 	public abstract String toString();
 	public abstract String toStringStore();
 	
+	
+	public void addToInventory() {
+		inventory_count++;
+	}
+	
+	public void removeFromInventory() {
+		inventory_count--;
+	}
+	
+	public int getInventoryCount() {
+		return inventory_count;
+	}
 	
     public void setPrice(int newPrice) {
     	price = newPrice;
