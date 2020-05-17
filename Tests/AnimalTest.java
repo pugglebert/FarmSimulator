4a0b82b7546bf00b65a0577688b2e582fc14c782
frostydj <64118807+frostydj@users.noreply.github.com>
2020-05-17 22:49:06 +1200

    Add files via upload

--- Tests/AnimalTest.java
+++ Tests/AnimalTest.java
@@ -1,4 +1,4 @@
-package test;
+package farmSimulatorTests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
--- Tests/SetupTest.java
+++ Tests/SetupTest.java
@@ -1,4 +1,4 @@
-package test;
+package farmSimulatorTests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,28 +17,28 @@ class SetupTest {
 	@ValueSource(strings = {"Bob", "Bob Smith", "abcdefghijklmno"})
 	void testValidName(String name) {
 		JLabel errorLabel = new JLabel();
-		assertTrue(setupWindow.checkName(name, errorLabel));
+		assertTrue(setupWindow.checkNameValid(name, errorLabel));
 	}
 	
 	@ParameterizedTest
 	@ValueSource(strings = {"Bo", "123", "abcdefghijklmnop", "Bob1", "B$!b"})
 	void testInvalidName(String name) {
 		JLabel errorLabel = new JLabel();
-		assertFalse(setupWindow.checkName(name, errorLabel));
+		assertFalse(setupWindow.checkNameValid(name, errorLabel));
 	}
 	
 	@ParameterizedTest
 	@ValueSource(strings = {"1", "100", "50"})
 	void testValidAge(String age) {
 		JLabel errorLabel = new JLabel();
-		assertTrue(setupWindow.checkAge(age, errorLabel));
+		assertTrue(setupWindow.checkAgeValid(age, errorLabel));
 	}
 	
 	@ParameterizedTest
 	@ValueSource(strings = {"0", "101", "5a", "@!1", "abc"})
 	void testInalidAge(String age) {
 		JLabel errorLabel = new JLabel();
-		assertFalse(setupWindow.checkAge(age, errorLabel));
+		assertFalse(setupWindow.checkAgeValid(age, errorLabel));
 	}
 	
 
