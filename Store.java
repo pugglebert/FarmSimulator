e471231806b9eb0c7f1ad53ffe5dc129a771701f
pugglebert <57479458+pugglebert@users.noreply.github.com>
2020-05-17 12:47:36 +1200

    Add files via upload

--- Store.java
+++ Store.java
@@ -2,63 +2,93 @@ package farmSimulatorGUI;
 
 import java.util.ArrayList;
 
+/**
+ * The store class provides lists Buyables which can be purchased using the buy method
+ * in the farm class.
+ * 
+ * @author David Frost, Ella Johnson
+ *
+ */
+
 public class Store {
 	
-	private ArrayList<FoodItem> foodItems = new ArrayList<FoodItem>();
-	private ArrayList<CropItem> cropItems = new ArrayList<CropItem>();
-	private ArrayList<Animal> animals = new ArrayList<Animal>();
-	private ArrayList<Crop> crops = new ArrayList<Crop>();
+	/**
+	 * List of food items available for purchase
+	 */
+	private ArrayList<Buyable> foodItems = new ArrayList<Buyable>();
 	
-	public ArrayList<Buyable> getFoodItems() {
-		ArrayList<Buyable> buyables = new ArrayList<Buyable>();
-		for (int i = 0; i < foodItems.size(); i++) {
-			Buyable buyableItem = (Buyable) foodItems.get(i);
-			buyables.add(buyableItem);
-		}
-		return buyables;
-	}
+	/**
+	 * List of crop items available for purchase
+	 */
+	private ArrayList<Buyable> cropItems = new ArrayList<Buyable>();
 	
+	/**
+	 * List of animals available for purchase
+	 */
+	private ArrayList<Buyable> animals = new ArrayList<Buyable>();
+	
+	/**
+	 * List of crops available for purchase
+	 */
+	private ArrayList<Buyable> crops = new ArrayList<Buyable>();
+	
+	/**
+	 * Converts arraylist of buyables to string representation
+	 * @param buyables one of the attributes of the store
+	 * @return array of string representations of buyables
+	 */
 	public String[] getStoreArray(ArrayList<Buyable> buyables) {
 		String[] buyArray = new String[buyables.size()];
 		for (int i = 0; i < buyables.size(); i++) {
 			buyArray[i] = buyables.get(i).toStringStore();
 		}
 		return buyArray;
+	}	
+	
+	/**
+	 * Returns ArrayList of food items that can be purchased cast as Buyable
+	 * @return list of food items for purchase
+	 */
+	public ArrayList<Buyable> getFoodItems() {
+		return foodItems;
 	}
 	
+	/**
+	 * Returns ArrayList of crop items that can be purchased cast as Buyable
+	 * @return list of crop items for purchase
+	 */
 	public ArrayList<Buyable> getCropItems() {
-		ArrayList<Buyable> buyables = new ArrayList<Buyable>();
-		for (int i = 0; i < cropItems.size(); i++) {
-			Buyable buyableItem = (Buyable) cropItems.get(i);
-			buyables.add(buyableItem);
-		}
-		return buyables;
+		return cropItems;
 	}
 	
+	/**
+	 * Returns ArrayList of animals that can be purchased cast as Buyable
+	 * @return list of animals for purchase
+	 */
 	public ArrayList<Buyable> getAnimals() {
-		ArrayList<Buyable> buyables = new ArrayList<Buyable>();
-		for (int i = 0; i < animals.size(); i++) {
-			Buyable buyableItem = (Buyable) animals.get(i);
-			buyables.add(buyableItem);
-		}
-		return buyables;
+		return animals;
 	}
 	
+	/**
+	 * Returns ArrayList of crops that can be purchased cast as Buyable
+	 * @return list of crops for purchase
+	 */
 	public ArrayList<Buyable> getCrops() {
-		ArrayList<Buyable> buyables = new ArrayList<Buyable>();
-		for (int i = 0; i < crops.size(); i++) {
-			Buyable buyableItem = (Buyable) crops.get(i);
-			buyables.add(buyableItem);
-		}
-		return buyables;
+		return crops;
 	}
 	
+	/**
+	 * Called by constructor, adds Animal objects to animals attribute
+	 */
 	private void initializeAnimals() {
 		animals.add(new Chicken());
 		animals.add(new Cow());
 		animals.add(new Sheep());
 	}
 	
+	/**
+	 * Called by constructor, adds Crop objects to crops attribute
+	 */
 	private void initializeCrops() {
 		crops.add(new Barley());
 		crops.add(new Kale());
@@ -68,17 +98,27 @@ public class Store {
 		crops.add(new Wheat());
 	}
 	
+	/**
+	 * Called by constructor, adds FoodItem objects to foodItems attribute
+	 */
 	private void initializeFItems() {
 		foodItems.add(new FoodItem("Small treat", 100, 1));
 		foodItems.add(new FoodItem("Big treat", 250, 3));
 		foodItems.add(new FoodItem("Super treat", 400, 6));
 	}
 	
+	/**
+	 * Called by constructor, adds CropItem objects to cropItems attribute
+	 */
 	private void initializeCItems() {
 		cropItems.add(new CropItem("Small fertilizer", 200, 1));
 		cropItems.add(new CropItem("Big fertilizer", 350, 2));
 		cropItems.add(new CropItem("Super fertilizer", 500, 3));
 	}
+	
+	/**
+	 * Store constructor, adds objects to empty ArrayList for each attribute
+	 */
 	public Store() {
 		initializeAnimals();
 		initializeCrops();
