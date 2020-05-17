131971b52aa0b9b0787a7782689035c29eb7f3b8
pugglebert <57479458+pugglebert@users.noreply.github.com>
2020-05-17 14:01:12 +1200

    Add files via upload

--- Farmer.java
+++ Farmer.java
@@ -2,36 +2,81 @@ package farmSimulatorGUI;
 
 import java.util.ArrayList;
 
+/**
+ * This class implements a Farmer. The Farmer owns a farm, money and items, and can be
+ * customized with a name and age
+ * @author David Frost, Ella Johnson
+ *
+ */
 public class Farmer {
 	
+	/**
+	 * Name of the farmer
+	 */
 	private String name;
+	
+	/**
+	 * The farmer's age
+	 */
 	private int age;
+	
+	/**
+	 * An ArrayList of all the items the Farmer owns
+	 */
 	private ArrayList<Item> items = new ArrayList<Item>();
 	
+	/**
+	 * Change Farmer's name
+	 * @param newName string to change name to
+	 */
 	public void setName(String newName) {
 		name = newName;
 	}
 	
+	/**
+	 * Returns Farmer's name
+	 * @return Farmer's name
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Change Farmer's age to new value
+	 * @param newAge value to change name to
+	 */
 	public void setAge(int newAge) {
 		age = newAge;
 	}
 	
+	/**
+	 * Return's Farmer's age
+	 * @return Farmer's age
+	 */
 	public int getAge() {
 		return age;
 	}
 	
+	/**
+	 * Sets list of Items in Farmer's possession to input
+	 * @param newItems ArrayList of Items to be owned by Farmer
+	 */
 	public void setItems(ArrayList<Item> newItems) {
 		items = newItems;
 	}
 	
+	/**
+	 * Returns list Items in Farmer's possession
+	 * @return List of Items Farmer owns
+	 */
 	public ArrayList<Item> getItems() {
 		return items;
 	}
 	
+	/**
+	 * Returns list of Items owned by Farmer which are of class FoodItem
+	 * @return List of FoodItems owned by Farmer
+	 */
 	public ArrayList<FoodItem> getFoodItems() {
 		ArrayList<FoodItem> foodItems = new ArrayList<FoodItem>();
 		for (Item item : items) {
@@ -42,6 +87,10 @@ public class Farmer {
 		return foodItems;
 	}
 	
+	/**
+	 * Returns list of Items owned by Farmer which are of class CropItem
+	 * @return List of CropItems owned by Farmer
+	 */
 	public ArrayList<CropItem> getCropItems() {
 		ArrayList<CropItem> cropItems = new ArrayList<CropItem>();
 		for (Item item : items) {
@@ -52,10 +101,18 @@ public class Farmer {
 		return cropItems;
 	}
 	
+	/**
+	 * Add an item which the Farmer does not already own to their list of Items
+	 * @param itemToAdd Item to be added to list
+	 */
 	public void addItem(Item itemToAdd) {
 		items.add(itemToAdd);
 	}
 	
+	/**
+	 * Remove an Item which the farmer owns 0 of from their list of Items
+	 * @param itemToRemove Item to be removed from list
+	 */
 	public void removeItem(Item itemToRemove) {
 		items.remove(itemToRemove);
 	}
--- Item.java
+++ Item.java
@@ -1,38 +1,88 @@
 package farmSimulatorGUI;
-
-abstract class Item implements Buyable {
+/**
+ * The abstract class Item can be bought and stored in a farmer's inventory
+ * Its subclasses foodItem and cropItem are used to provide bonuses to crops
+ * and animals respectively
+ * @author David Frost, Ella Johnson
+ *
+ */
+public abstract class Item implements Buyable {
 	
+	/**
+	 * Items name used as part of string representation
+	 */
 	private String name;
+	
+	/**
+	 * Price for which Item can be bought
+	 */
 	private int price;
+	
+	/**
+	 * Number of a particular type of item which farmer has in their possession
+	 */
 	private int inventory_count = 0;
+	
+	/**
+	 * String representation used in inventory screen
+	 */
 	public abstract String toString();
+	
+	/**
+	 * String representation used in store screen
+	 */
 	public abstract String toStringStore();
 	
-	
+	/**
+	 * Increase amount of an Item which the farmer owns by 1
+	 */
 	public void addToInventory() {
 		inventory_count++;
 	}
 	
+	/**
+	 * Decreases amount of the Item that the farmer owns by 1
+	 */
 	public void removeFromInventory() {
 		inventory_count--;
 	}
 	
+	/**
+	 * Returns how many of a particular item are owned
+	 * @return number of specific Item owned by farmer
+	 */
 	public int getInventoryCount() {
 		return inventory_count;
 	}
 	
+	/**
+	 * Set price Item can be bought for to new value
+	 * @param newPrice price Item can be bought for
+	 */
     public void setPrice(int newPrice) {
     	price = newPrice;
     }
     
+    /**
+     * Returns price Item can be bought for
+     * @return price Item can be bought for
+     */
     public int getBuyPrice() {
     	return price;
     }
     
+    /**
+     * Sets name of Item to new string
+     * @param newName string to set name to
+     */
     public void setName(String newName) {
     	name = newName;
     }
     
+    /**
+     * Returns name of Item
+     * @return name of Item
+     */
     public String getName() {
     	return name;
     }
