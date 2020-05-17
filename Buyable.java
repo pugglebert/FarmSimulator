f74e5a0e29b538b85259320ff84df4a81cfc2180
pugglebert <57479458+pugglebert@users.noreply.github.com>
2020-05-17 16:27:37 +1200

    Add files via upload

--- Buyable.java
+++ Buyable.java
@@ -1,8 +1,24 @@
 package farmSimulatorGUI;
 
+/**
+ * Interface Buyable allows objects which implement it to be displayed
+ * and bought in store
+ * @author David Frost, Ella Johnson
+ *
+ */
 public interface Buyable {
 	
+	/**
+	 * Return price for which object can be purchased
+	 * @return purchase price
+	 */
 	public int getBuyPrice();
+	
+	/**
+	 * Returns string of object description and price to be displayed
+	 * in store screen
+	 * @return String of object description and price
+	 */
 	public String toStringStore();
 	
 }
