a73c99c1ee6eb9d6d1f1e62db8e7a9928d278f7b
pugglebert <57479458+pugglebert@users.noreply.github.com>
2020-05-17 12:14:01 +1200

    Add files via upload

---
+++ Tests/StoreTest.java
@@ -0,0 +1,57 @@
+package farmSimulatorTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import farmSimulatorGUI.*;
+
+class StoreTest {
+
+	@Test
+	final void testGetStoreArray() {
+		Store testStore = new Store();
+		ArrayList<Buyable> testBuyables = new ArrayList<Buyable>();
+		Buyable testCow = new Cow();
+		testBuyables.add(testCow);
+		Buyable testItem = new CropItem("Test", 1, 1);
+		testBuyables.add(testItem);
+		String[] testArray = testStore.getStoreArray(testBuyables);
+		assertEquals(2, testArray.length);
+		assertEquals(testCow.toStringStore(), testArray[0]);
+	}
+
+	@Test
+	final void testGetFoodItems() {
+		Store testStore = new Store();
+		assertEquals(3, testStore.getFoodItems().size());
+		FoodItem testItem = new FoodItem("Big treat", 250, 3);
+		assertEquals(testItem.toString(), testStore.getFoodItems().get(1).toString());
+	}
+
+	@Test
+	final void testGetCropItems() {
+		Store testStore = new Store();
+		assertEquals(3, testStore.getCropItems().size());
+		CropItem testItem = new CropItem("Small fertilizer", 200, 1);
+		assertEquals(testItem.toString(), testStore.getCropItems().get(0).toString());
+	}
+
+	@Test
+	final void testGetAnimals() {
+		Store testStore = new Store();
+		assertEquals(3, testStore.getAnimals().size());
+		Animal testAnimal = new Sheep();
+		assertEquals(testAnimal.toStringStore(), testStore.getAnimals().get(2).toStringStore());
+	}
+
+	@Test
+	final void testGetCrops() {
+		Store testStore = new Store();
+		assertEquals(6, testStore.getCrops().size());
+		Crop testCrop = new Wheat();
+		assertEquals(testCrop.toStringStore(), testStore.getCrops().get(5).toStringStore());
+	}
+
+}
