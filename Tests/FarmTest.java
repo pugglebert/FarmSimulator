a34f68c931d20d42051800e77f9b6d0261a31046
frostydj <64118807+frostydj@users.noreply.github.com>
2020-05-23 20:13:43 +1200

    Update FarmTest.java

--- Tests/FarmTest.java
+++ Tests/FarmTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import CustomExceptions.InvalidFarmNameException;
 import farmSimulatorGUI.*;
 import java.util.ArrayList;
 
